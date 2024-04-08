5bc56502c60115dbb0010fcf956ef0d8300753f3
AgentGarrus <spektr589@gmail.com>
2024-04-08 18:33:38 +0200

    5

--- src/main.js
+++ src/main.js
@@ -41,6 +41,7 @@ searchForm.addEventListener('submit', async (event) => {
 });
 
 function renderGallery(images) {
+  const gallery = document.querySelector('.gallery');
   gallery.innerHTML = '';
   
   images.forEach(image => {
@@ -62,9 +63,23 @@ function createImageCard(image) {
   const img = document.createElement('img');
   img.src = image.webformatURL;
   img.alt = image.tags;
-  
-  imageLink.appendChild(img);
-  card.appendChild(imageLink);
-  
+  card.appendChild(img);
+
+ const stats = document.createElement('div');
+  stats.classList.add('stats');
+  const likes = document.createElement('span');
+  likes.textContent = `Likes: ${image.likes}`;
+  stats.appendChild(likes);
+  const views = document.createElement('span');
+  views.textContent = `Views: ${image.views}`;
+  stats.appendChild(views);
+  const comments = document.createElement('span');
+  comments.textContent = `Comments: ${image.comments}`;
+  stats.appendChild(comments);
+  const downloads = document.createElement('span');
+  downloads.textContent = `Downloads: ${image.downloads}`;
+  stats.appendChild(downloads);
+  card.appendChild(stats);
+
   return card;
 }
